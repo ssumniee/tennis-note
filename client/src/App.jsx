b8f830defd2cf3b9b01290adabadff2949487034
ssumniee <suemink.dev@gmail.com>
2021-12-01 14:53:26 +0900

    [client] feat: Set up routing structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div>Tennis Note!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route path="/home" component={Home} />
+        <Route />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
