e1f23a03401428f9800432d83f7ddc3c3813c3de
ssumniee <suemink.dev@gmail.com>
2021-12-01 16:04:59 +0900

    [client] feat: Add redirection logic when user is logged out

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const { isLogin } = useSelector(({ authReducer }) => authReducer);
+
+  useEffect(() => {
+    navigate("/", { replace: true });
+  }, [isLogin]);
+
   return <div></div>;
 };
 
