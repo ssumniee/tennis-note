20fd181c75e163b6f05edfe73ad3639a08fa3088
ssumniee <suemink.dev@gmail.com>
2021-12-02 11:45:28 +0900

    [server] feat: Set up auth controller

---
+++ server/controllers/auth.js
@@ -0,0 +1,34 @@
+// TODO: 비밀번호 암호화하기
+// const bcrypt = require("bcrypt");
+// const {
+//   bcrypt: { saltRounds },
+// } = require("../config");
+const { findOneClub } = require("./functions/sequelize");
+const { DBERROR } = require("./functions/utility");
+const { clearCookie, generateAccessToken, setCookie } = require("./functions/token");
+
+module.exports = {
+  login: async (req, res) => {
+    try {
+      const { name, password } = req.body;
+      const clubAccount = await findOneClub({ name, password });
+      if (!clubAccount) {
+        return res.status(401).json({ message: "유효하지 않은 아이디 또는 비밀번호입니다" });
+      }
+      const token = generateAccessToken(clubAccount.dataValues.id);
+      setCookie(res, token);
+      const { id, tel } = clubAccount.dataValues;
+      return res.status(200).json({ id, name, tel });
+    } catch (err) {
+      DBERROR(res, err);
+    }
+  },
+  logout: async (req, res) => {
+    try {
+      clearCookie(res);
+      return res.status(200).json({ message: "로그아웃 되었습니다." });
+    } catch (err) {
+      DBERROR(res, err);
+    }
+  },
+};
