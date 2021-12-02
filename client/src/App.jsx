47a3dc14446e4abd208cc28e55e96e1b4f0653b0
ssumniee <suemink.dev@gmail.com>
2021-12-02 11:42:49 +0900

    [client] feat: Update App.jsx routing structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
+import Landing from "./pages/Signing";
 import Home from "./pages/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" exact element={<Landing />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
         <Route />
       </Routes>
