3b7451fda6d57248330a91c335b146b319a1f1cb
ssumniee <suemink.dev@gmail.com>
2021-12-02 15:48:57 +0900

    [client] feat: Add Header component to Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Signing";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/home"
+          element={
+            <>
+              <Header />
+              <Home />
+            </>
+          }
+        />
         <Route />
       </Routes>
     </BrowserRouter>
