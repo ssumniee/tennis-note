8493e04966b19a750ee2bcbd0d5f0b31c002e1bf
ssumniee <suemink.dev@gmail.com>
2021-12-05 15:02:44 +0900

    [client] feat: Create user info modifying api function

--- client/src/api/club.js
+++ client/src/api/club.js
@@ -1,9 +1,17 @@
 import api from "./index";
 
 const clubApi = {
-  getAllUserInfo: async (id) => {
+  getAllUserInfo: async (clubId) => {
     try {
-      const res = await api.get(`/club/${id}`);
+      const res = await api.get(`/club/${clubId}`);
+      if (res.status === 200) return res;
+    } catch (err) {
+      console.error(err);
+    }
+  },
+  modifyUserInfo: async (clubId, userInfo) => {
+    try {
+      const res = await api.put(`/club/${clubId}`, { updated: { ...userInfo } });
       if (res.status === 200) return res;
     } catch (err) {
       console.error(err);
