ccaed6a2ab7032f335b98901d35e2fe440439745
ssumniee <suemink.dev@gmail.com>
2021-12-10 23:53:33 +0900

    [client] fix: Update user verifying process

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -35,8 +35,10 @@ const Home = () => {
       } catch (error) {
         // 토큰 만료의 경우 error.response.status가 403인 경우를 조건으로 -> 로그아웃 후 Signing으로 리디렉션
         // 다만, res.data가 제대로 넘어오지 않은 경우 등의 예외적인 에러에 대해서도 로그아웃 후 리디렉션 시키기 위해 조건문 생략
-        dispatch(logoutAction);
-        navigate("/", { replace: true });
+        if (error.response.status === 403) {
+          dispatch(logoutAction);
+          navigate("/", { replace: true });
+        }
       }
     };
     checkValidUser();
