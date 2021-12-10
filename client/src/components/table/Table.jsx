fb82a358f3a0707266e5f4046db874d65d29a8a4
ssumniee <suemink.dev@gmail.com>
2021-12-10 23:35:20 +0900

    [client] style: Remove console log printing line

--- client/src/components/table/Table.jsx
+++ client/src/components/table/Table.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import Row from "./Row";
@@ -22,9 +22,6 @@ const TableBody = styled.tbody`
 `;
 
 const Table = ({ users }) => {
-  useEffect(() => {
-    console.log("user updated");
-  }, []);
   return (
     <TableContainer>
       <TableHead>
