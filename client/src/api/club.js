2cae0068d1f80ce355c1b2f8c865164021b6adaf
ssumniee <suemink.dev@gmail.com>
2021-12-21 15:35:49 +0900

    [client] style: Update api functions

--- client/src/api/club.js
+++ client/src/api/club.js
@@ -1,14 +1,7 @@
 import api from "./index";
 
 const clubApi = {
-  getAllClubInfo: async () => {
-    try {
-      const res = await api.get("/club");
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
+  getAllClubInfo: () => api.get("/club"),
   getOneClubId: async (queries) => {
     try {
       const queryString = Object.keys(queries)
@@ -20,114 +13,33 @@ const clubApi = {
       console.error(err);
     }
   },
-  modifyClubInfo: async (clubInfo) => {
-    try {
-      const res = await api.put("/club", { updated: { ...clubInfo } });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  checkClubNameUniqueness: async (name) => {
-    try {
-      const res = await api.post("/club/name", { name });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  modifyClubPassword: async ({ id, password }) => {
-    try {
-      const res = await api.put("/club/password", { id, password });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  resetClubPassword: async ({ id, password }) => {
-    try {
-      const res = await api.post("/club/password", { id, password });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  addClubInfo: async (newClubInfo) => {
-    try {
-      const res = await api.post("/club", { ...newClubInfo });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  deleteClubInfo: async (clubId) => {
-    try {
-      const res = await api.delete("/club", {
-        data: {
-          id: clubId,
-        },
-      });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  modifyTeacherInfo: async (teacherInfo) => {
-    try {
-      const res = await api.put("/club/teacher", { updated: { ...teacherInfo } });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  addTeacherInfo: async (newTeacherInfo) => {
-    try {
-      const res = await api.post("/club/teacher", { ...newTeacherInfo });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  deleteTeacherInfo: async (teacherId) => {
-    try {
-      const res = await api.delete("/club/teacher", {
-        data: {
-          id: teacherId,
-        },
-      });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  modifyCourtInfo: async (courtInfo) => {
-    try {
-      const res = await api.put("/club/court", { updated: { ...courtInfo } });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  addCourtInfo: async (newCourtInfo) => {
-    try {
-      const res = await api.post("/club/court", { ...newCourtInfo });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
-  deleteCourtInfo: async (courtId) => {
-    try {
-      const res = await api.delete("/club/court", {
-        data: {
-          id: courtId,
-        },
-      });
-      if (res.status === 200) return res;
-    } catch (err) {
-      console.error(err);
-    }
-  },
+  modifyClubInfo: (clubInfo) => api.put("/club", { updated: { ...clubInfo } }),
+  checkClubNameUniqueness: (name) => api.post("/club/name", { name }),
+  modifyClubPassword: ({ id, password }) => api.put("/club/password", { id, password }),
+  resetClubPassword: ({ id, password }) => api.post("/club/password", { id, password }),
+  addClubInfo: (newClubInfo) => api.post("/club", { ...newClubInfo }),
+  deleteClubInfo: (clubId) =>
+    api.delete("/club", {
+      data: {
+        id: clubId,
+      },
+    }),
+  modifyTeacherInfo: (teacherInfo) => api.put("/club/teacher", { updated: { ...teacherInfo } }),
+  addTeacherInfo: (newTeacherInfo) => api.post("/club/teacher", { ...newTeacherInfo }),
+  deleteTeacherInfo: (teacherId) =>
+    api.delete("/club/teacher", {
+      data: {
+        id: teacherId,
+      },
+    }),
+  modifyCourtInfo: (courtInfo) => api.put("/club/court", { updated: { ...courtInfo } }),
+  addCourtInfo: (newCourtInfo) => api.post("/club/court", { ...newCourtInfo }),
+  deleteCourtInfo: (courtId) =>
+    api.delete("/club/court", {
+      data: {
+        id: courtId,
+      },
+    }),
 };
 
 export default clubApi;
