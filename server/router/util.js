682bcd6687c6c53a44edbe866b2b4a545267804e
ssumniee <suemink.dev@gmail.com>
2021-12-21 15:23:43 +0900

    [server] fix: Update router setting for password resetting process

--- server/router/util.js
+++ server/router/util.js
@@ -4,6 +4,6 @@ const { isAuth } = require("../middlewares");
 const { getTempPassword, sendVerificationSMS } = require("../controllers/util");
 
 router.get("/password", isAuth, getTempPassword);
-router.post("/message/code", isAuth, sendVerificationSMS);
+router.post("/message/code", sendVerificationSMS);
 
 module.exports = router;
