f80f1ac516062cfbe08b36059b3a626a9f698e75
ssumniee <suemink.dev@gmail.com>
2021-12-24 20:13:09 +0900

    [server] feat: Update SMS sending process using coolsms

--- server/controllers/functions/utility.js
+++ server/controllers/functions/utility.js
@@ -1,3 +1,6 @@
+const { msg, config } = require("coolsms-node-sdk");
+const { coolsms } = require("../../config");
+
 module.exports = {
   DBERROR: (res, err) => {
     return res.status(500).json({ message: `DB 에러 발생: ${err}` });
@@ -7,5 +10,21 @@ module.exports = {
   },
   createRandomNumber: (len = 6) => {
     return Math.random().toFixed(len).toString().slice(2);
+  },
+  sendMessage: async (telList, text) => {
+    try {
+      const { apiKey, apiSecret, callingNumber } = coolsms;
+      config.init({
+        apiKey: apiKey,
+        apiSecret: apiSecret,
+      });
+
+      const msgRes = await msg.send({
+        messages: telList.map((tel) => ({ to: tel, from: callingNumber, text })),
+      });
+      console.log(msgRes);
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
--- server/controllers/util.js
+++ server/controllers/util.js
@@ -1,7 +1,4 @@
-const { createRandomString, createRandomNumber } = require("./functions/utility");
-const { sens } = require("../config");
-const CryptoJS = require("crypto-js");
-const axios = require("axios");
+const { createRandomString, createRandomNumber, sendMessage } = require("./functions/utility");
 
 module.exports = {
   getTempPassword: async (req, res) => {
@@ -15,57 +12,16 @@ module.exports = {
   sendVerificationSMS: async (req, res) => {
     try {
       const { tel } = req.body;
-      const user_phone_number = tel.split("-").join(""); // SMS를 수신할 전화번호
+      const destNumber = tel.split("-").join(""); // SMS를 수신할 전화번호
       const verificationCode = createRandomNumber(6); // 인증 코드 (6자리 숫자)
-      const date = Date.now().toString(); // 날짜 string
+      const text = `[테니스노트] 인증번호 [${verificationCode}]를 입력해주세요.`;
 
-      // 환경 변수
-      const sens_service_id = sens.serviceId;
-      const sens_access_key = sens.accessKey;
-      const sens_secret_key = sens.secretKey;
-      const sens_call_number = sens.callNumber;
+      sendMessage([destNumber], text);
 
-      // url 관련 변수 선언
-      const method = "POST";
-      const space = " ";
-      const newLine = "\n";
-      const url = `https://sens.apigw.ntruss.com/sms/v2/services/${sens_service_id}/messages`;
-      const url2 = `/sms/v2/services/${sens_service_id}/messages`;
-
-      // signature 작성 : crypto-js 모듈을 이용하여 암호화
-      const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, sens_secret_key);
-      hmac.update(method);
-      hmac.update(space);
-      hmac.update(url2);
-      hmac.update(newLine);
-      hmac.update(date);
-      hmac.update(newLine);
-      hmac.update(sens_access_key);
-      const hash = hmac.finalize();
-      const signature = hash.toString(CryptoJS.enc.Base64);
-
-      // sens 서버로 요청 전송
-      await axios({
-        method: method,
-        url: url,
-        headers: {
-          "Contenc-type": "application/json; charset=utf-8",
-          "x-ncp-iam-access-key": sens_access_key,
-          "x-ncp-apigw-timestamp": date,
-          "x-ncp-apigw-signature-v2": signature,
-        },
-        data: {
-          type: "SMS",
-          countryCode: "82",
-          from: sens_call_number,
-          content: `[테니스노트] 본인확인 인증번호 [${verificationCode}]를 입력해주세요.`,
-          messages: [{ to: `${user_phone_number}` }],
-        },
-      });
-      return res.status(202).json({ message: "SMS sent", verificationCode });
+      return res.status(201).json({ message: "Verification SMS sent", verificationCode });
     } catch (err) {
       console.log(err);
-      return res.status(404).json({ message: "SMS not sent" });
+      return res.status(404).json({ message: "Verification SMS not sent" });
     }
   },
 };
