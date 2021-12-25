28fc9087524a5aa0e0e3674efb0256de4a119722
ssumniee <suemink.dev@gmail.com>
2021-12-25 11:50:14 +0900

    [server] chore: Update scripts start.sh env

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,9 +11,8 @@ export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names AC
 export BCRYPT_SALT_ROUNDS=$(aws ssm get-parameters --region ap-northeast-2 --names BCRYPT_SALT_ROUNDS --query Parameters[0].Value | sed 's/"//g')
 export JWT_EXPIRES_SEC=$(aws ssm get-parameters --region ap-northeast-2 --names JWT_EXPIRES_SEC --query Parameters[0].Value | sed 's/"//g')
 export CORS_ALLOW_ORIGIN=$(aws ssm get-parameters --region ap-northeast-2 --names CORS_ALLOW_ORIGIN --query Parameters[0].Value | sed 's/"//g')
-export NCP_SENS_ACCESS=$(aws ssm get-parameters --region ap-northeast-2 --names NCP_SENS_ACCESS --query Prameters[0].Value | sed 's/"//g')
-export NCP_SENS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names NCP_SENS_SECRET --query Prameters[0].Value | sed 's/"//g')
-export NCP_SENS_ID=$(aws ssm get-parameters --region ap-northeast-2 --names NCP_SENS_ID --query Prameters[0].Value | sed 's/"//g')
-export NCP_SENS_NUMBER=$(aws ssm get-parameters --region ap-northeast-2 --names NCP_SENS_NUMBER --query Prameters[0].Value | sed 's/"//g')
+export COOLSMS_API_KEY=$(aws ssm get-parameters --region ap-northeast-2 --names COOLSMS_API_KEY --query Prameters[0].Value | sed 's/"//g')
+export COOLSMS_API_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names COOLSMS_API_SECRET --query Prameters[0].Value | sed 's/"//g')
+export COOLSMS_CALLING_NUMBER=$(aws ssm get-parameters --region ap-northeast-2 --names COOLSMS_CALLING_NUMBER --query Prameters[0].Value | sed 's/"//g')
 
 authbind --deep pm2 start app.js
