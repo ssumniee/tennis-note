530c5fd180bf3ff30e857e3ff5f2d4f844f2f76d
ssumniee <suemink.dev@gmail.com>
2021-12-26 19:27:04 +0900

    [client] feat: Update data import/export buttons' structure & UI

--- client/src/pages/Student.jsx
+++ client/src/pages/Student.jsx
@@ -7,7 +7,7 @@ import authApi from "../api/auth";
 import studentApi from "../api/student";
 import { loginAction, logoutAction, getAllStudentInfoAction } from "../store/actions";
 import Table from "../components/table/Table";
-import ImportExportBtnContainer from "../components/ImportExportBtnContainer";
+import TableTopBar from "../components/table/TableTopBar";
 
 const StudentContainer = styled.div`
   width: 100vw;
@@ -56,7 +56,7 @@ const Student = () => {
 
   return (
     <StudentContainer>
-      <ImportExportBtnContainer subject="student" />
+      <TableTopBar subject="student" />
       <Table subject="student" infoList={studentList} />
       <Table subject="student" isAdding />
     </StudentContainer>
