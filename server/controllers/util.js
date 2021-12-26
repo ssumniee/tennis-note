1bd0c0048a3e68aeaa3d233b23d9255f610a372f
ssumniee <suemink.dev@gmail.com>
2021-12-26 17:48:45 +0900

    [server] feat: Refine SMS sending process & repay noti SMS sending logic

--- server/controllers/util.js
+++ server/controllers/util.js
@@ -1,4 +1,5 @@
 const { createRandomString, createRandomNumber, sendMessage } = require("./functions/utility");
+const { findStudentToRepayByClubId, updateStudentInfo } = require("./functions/sequelize");
 
 module.exports = {
   getTempPassword: async (req, res) => {
@@ -15,21 +16,30 @@ module.exports = {
       const destNumber = tel.split("-").join(""); // SMS를 수신할 전화번호
       const verificationCode = createRandomNumber(6); // 인증 코드 (6자리 숫자)
       const text = `[테니스노트] 인증번호 [${verificationCode}]를 입력해주세요.`;
-
-      sendMessage([destNumber], text);
-
+      // SMS 전송
+      sendMessage([{ tel: destNumber, text }]);
       return res.status(201).json({ message: "Verification SMS sent", verificationCode });
     } catch (err) {
       console.log(err);
       return res.status(404).json({ message: "Verification SMS not sent" });
     }
   },
-  sendRepaymentSMS: ({ club, toSend }) => {
+  sendRepaymentSMS: async (clubInfo) => {
     try {
-      const telList = toSend.map((item) => item.tel.split("-").join("")); // SMS를 수신할 전화번호 목록
-      const text = `[${club.clubname}] 결제일이 되었습니다. 다음 수업 전에 결제해주세요.`;
-
-      sendMessage(telList, text);
+      const rawList = await findStudentToRepayByClubId(clubInfo.id);
+      const toSendList = rawList.map((item) => ({
+        tel: item.tel.split("-").join(""), // SMS를 수신할 전화번호
+        text: `[${clubInfo.clubname}] ${item.name} 회원님의 레슨 결제주간입니다. 확인 부탁드립니다.`, // SMS 내용
+      }));
+      // SMS 전송
+      sendMessage(toSendList);
+      // repay SMS 전송 여부 갱신
+      const sentList = [];
+      rawList.forEach(async (item) => {
+        await updateStudentInfo({ id: item.id, repayNotiSent: true });
+        sentList.push({ id: item.id });
+      });
+      console.log({ message: "repay noti SMS sent", sent: sentList });
     } catch (err) {
       console.log(err);
     }
