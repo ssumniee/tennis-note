f377c45543b1a20c2c430ae8bc8ee7b2804cc5ed
ssumniee <suemink.dev@gmail.com>
2021-12-27 17:17:00 +0900

    [client] feat: Update table data exporting process

--- client/src/components/table/ExportBtn.jsx
+++ client/src/components/table/ExportBtn.jsx
@@ -27,42 +27,62 @@ const ExportBtn = ({ subject, infoList }) => {
   } = useSelector(({ authReducer }) => authReducer);
 
   const exportTableData = () => {
-    // 데이터 가공
-    const dataList = [];
-    infoList.forEach((info) => {
-      const data = {};
-      for (const prop in heads[subject]) {
-        data[heads[subject][prop]] = info[prop];
+    const wb = xlsx.utils.book_new(); // 워크북 생성
+    if (subject === "club") {
+      // 데이터 가공 & 워크시트 생성
+      const dataList = { admin: [], temp: [], club: [] };
+      const ws = {};
+      for (const infoProp in infoList) {
+        infoList[infoProp].forEach((info) => {
+          const data = {};
+          for (const prop in heads.club) {
+            data[heads.club[prop]] = info[prop];
+          }
+          dataList[infoProp].push(data);
+        });
+        ws[infoProp] = xlsx.utils.json_to_sheet(dataList[infoProp]);
+        xlsx.utils.book_append_sheet(wb, ws[infoProp], `${infoProp}`);
       }
-      dataList.push(data);
-    });
-    switch (subject) {
-      case "student":
-        dataList.forEach((data) => {
-          const teacherId = data["선생님"];
-          data["선생님"] = teacherList.find((teacher) => teacher.id === teacherId)?.name || "";
-          const dayIds = data["요일"];
-          data["요일"] = dayIds.map((id) => dayList.find((day) => day.id === id)?.name).join(", ");
-        });
-        break;
-      case "teacher":
-        dataList.forEach((data) => {
-          const courtId = data["코트"];
-          data["코트"] = courtList.find((court) => court.id === courtId)?.name || "";
-        });
-        break;
-      default:
-        break;
+    } else {
+      // 데이터 가공
+      const dataList = [];
+      infoList.forEach((info) => {
+        const data = {};
+        for (const prop in heads[subject]) {
+          data[heads[subject][prop]] = info[prop];
+        }
+        dataList.push(data);
+      });
+      switch (subject) {
+        case "student":
+          dataList.forEach((data) => {
+            const teacherId = data["선생님"];
+            data["선생님"] = teacherList.find((teacher) => teacher.id === teacherId)?.name || "";
+            const dayIds = data["요일"];
+            data["요일"] = dayIds
+              .map((id) => dayList.find((day) => day.id === id)?.name)
+              .join(", ");
+          });
+          break;
+        case "teacher":
+          dataList.forEach((data) => {
+            const courtId = data["코트"];
+            data["코트"] = courtList.find((court) => court.id === courtId)?.name || "";
+          });
+          break;
+        default:
+          break;
+      }
+      // 워크시트 생성
+      const ws = xlsx.utils.json_to_sheet(dataList);
+      xlsx.utils.book_append_sheet(wb, ws, `${subject}`);
     }
-    // 엑셀 파일로 다운로드
-    const ws = xlsx.utils.json_to_sheet(dataList);
-    const wb = xlsx.utils.book_new();
-    xlsx.utils.book_append_sheet(wb, ws, `${subject}`);
+    // [yyyyMMdd]-[subject(복수형)] 형태로 파일명 생성
     const today = new Date();
     const fileName = `${today.getFullYear()}${
       today.getMonth() + 1
-    }${today.getDate()}-${subject}.xlsx`;
-    xlsx.writeFile(wb, fileName);
+    }${today.getDate()}-${subject}s.xlsx`;
+    xlsx.writeFile(wb, fileName); // 엑셀 파일 다운로드
   };
 
   return (
@@ -75,24 +95,34 @@ const ExportBtn = ({ subject, infoList }) => {
 
 ExportBtn.propTypes = {
   subject: PropTypes.string,
-  infoList: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      num: PropTypes.number,
-      name: PropTypes.string,
-      username: PropTypes.string,
-      clubname: PropTypes.string,
-      club_id: PropTypes.number,
-      teacher_id: PropTypes.number,
-      tel: PropTypes.string,
-      start_date: PropTypes.string,
-      count: PropTypes.number,
-      days: PropTypes.arrayOf(PropTypes.number),
-      password: PropTypes.string,
-      createdAt: PropTypes.date,
-      court_id: PropTypes.number,
-    })
-  ),
+  infoList: PropTypes.oneOfType([
+    PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        num: PropTypes.number,
+        name: PropTypes.string,
+        club_id: PropTypes.number,
+        teacher_id: PropTypes.number,
+        tel: PropTypes.string,
+        start_date: PropTypes.string,
+        count: PropTypes.number,
+        days: PropTypes.arrayOf(PropTypes.number),
+        court_id: PropTypes.number,
+      })
+    ),
+    PropTypes.objectOf(
+      PropTypes.arrayOf(
+        PropTypes.shape({
+          id: PropTypes.number,
+          num: PropTypes.number,
+          username: PropTypes.string,
+          clubname: PropTypes.string,
+          password: PropTypes.string,
+          createdAt: PropTypes.date,
+        })
+      )
+    ),
+  ]),
 };
 
 export default ExportBtn;
